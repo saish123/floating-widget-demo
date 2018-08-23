7aa857588e3c55ecc8a1f649d43790f66dd5e935
SAISH SAWANT <saish1233@gmail.com>
2018-08-23 16:52:56 +0530

    Hiding TitleBar of activity

--- app/src/main/java/info/androidhive/floatingview/DialogActivity.java
+++ app/src/main/java/info/androidhive/floatingview/DialogActivity.java
@@ -1,28 +1,28 @@
 package info.androidhive.floatingview;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class DialogActivity extends AppCompatActivity {
+public class DialogActivity extends Activity {
 
     private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_dialog);
 
         okButton = (Button) findViewById(R.id.okButton);
-
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DialogActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+                Toast.makeText(DialogActivity.this, "Bye", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
